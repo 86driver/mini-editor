7e73237dcbb95908eaf783b90c400eeb1666a919
yanb <610410574@qq.com>
2020-10-14 20:38:25 +0800

    降低css-loader版本

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,12 @@ import {
   domCreateElement,
   domCreateDocumentFragment
 } from './utils/dom'
-
+import './assets/styles/test.css'
 function MiniEditor(el: string, options?: object) {
   let domFragment = domCreateDocumentFragment()
   let toolbarDom = domCreateElement('div')
   toolbarDom.innerHTML = '这里是toolbar区域'
+  toolbarDom.classList.add('toolbar')
   let contentDom = domCreateElement('div')
   contentDom.innerHTML = '这里是可编辑区域'
   contentDom.setAttribute('contenteditable', 'true')
